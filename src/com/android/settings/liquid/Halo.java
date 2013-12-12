b85261bbc53b1db6e63f7c25820856845f73f32f
Sean Hoyt <seanhoyt963@gmail.com>
2013-12-11 21:26:20 -0600

    Fix Halo derp

--- src/com/android/settings/liquid/Halo.java
+++ src/com/android/settings/liquid/Halo.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.crdroid;
+package com.android.settings.liquid;
 
 import android.app.ActivityManager;
 import android.app.INotificationManager;
