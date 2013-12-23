536a776a69bc27b4a627bf041c531c4b097892ab
Jubakuba <Jubakuba@gmail.com>
2013-12-22 21:51:58 -0700

    Settings: QuickRecord Tile (2/2)
    PS2: Forgot to set image name.
    
    Change-Id: Ib20d4c7231f147674ed3afd4c438bd83aedbac1d
    
    Conflicts:
    	src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java

--- src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
@@ -36,6 +36,7 @@ import static com.android.internal.util.liquid.QSConstants.TILE_MOBILEDATA;
 import static com.android.internal.util.liquid.QSConstants.TILE_MUSIC;
 import static com.android.internal.util.liquid.QSConstants.TILE_NETWORKMODE;
 import static com.android.internal.util.liquid.QSConstants.TILE_NFC;
+import static com.android.internal.util.liquid.QSConstants.TILE_QUICKRECORD;
 import static com.android.internal.util.liquid.QSConstants.TILE_QUIETHOURS;
 import static com.android.internal.util.liquid.QSConstants.TILE_RINGER;
 import static com.android.internal.util.liquid.QSConstants.TILE_SCREENTIMEOUT;
@@ -154,6 +155,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_THEME, R.string.title_tile_theme,
                 "com.android.systemui:drawable/ic_qs_theme_manual"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_QUICKRECORD, R.string.title_tile_quick_record,
+                "com.android.systemui:drawable/ic_qs_quickrecord"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
