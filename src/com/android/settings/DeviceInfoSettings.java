efd7c1fda1f575856485e24be05f6e19450d83d6
liquid0624 <rjbirnesser@gmail.com>
2014-01-27 13:53:01 -0500

    Remove unused file stream import

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -32,7 +32,6 @@ import android.util.Log;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
