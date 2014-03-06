b258d732d085b29c7c701e8461d5114b9345e49f
Jason H Weitzel <jsnweitzel@gmail.com>
2014-03-06 11:30:42 -0500

    fix recents switcher 	modified:   src/com/android/settings/liquid/InterfaceSettings.java

--- src/com/android/settings/liquid/InterfaceSettings.java
+++ src/com/android/settings/liquid/InterfaceSettings.java
@@ -46,7 +46,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 
-import com.android.settings.liquid.util.CMDProcessor;
 import com.android.settings.liquid.util.Helpers;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -70,7 +69,7 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements
     private static Activity mActivity;
     private CheckBoxPreference mUseAltResolver;
     private static ListPreference mLcdDensity;
-	private CheckBoxPreference mRecentsCustom;
+    private CheckBoxPreference mRecentsCustom;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -85,7 +84,7 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements
                 .getContentResolver(), Settings.System.ACTIVITY_RESOLVER_USE_ALT, 0) == 1);
         mUseAltResolver.setOnPreferenceChangeListener(this);
 
-        boolean enableRecentsCustom = Settings.System.getIntForUser(getActivity().getContentResolver(),
+        boolean enableRecentsCustom = Settings.System.getBoolean(getActivity().getContentResolver(),
                                       Settings.System.CUSTOM_RECENT, false);
         mRecentsCustom = (CheckBoxPreference) findPreference(CUSTOM_RECENT_MODE);
         mRecentsCustom.setChecked(enableRecentsCustom);
@@ -127,9 +126,9 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements
             }
             return true;
         } else if (preference == mRecentsCustom) { // Enable||disable Slim Recent
-            Settings.System.getIntForUser(getActivity().getContentResolver(),
+            Settings.System.putBoolean(getActivity().getContentResolver(),
                     Settings.System.CUSTOM_RECENT,
-                    (Boolean) newValue ? 1 : 0);
+                    ((Boolean) newValue) ? true : false);
             Helpers.restartSystemUI();
             return true;
         }
