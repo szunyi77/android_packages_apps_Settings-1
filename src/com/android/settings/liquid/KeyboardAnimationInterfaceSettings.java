ssawrav/packages_apps_Settings

Settings: IME animations (2/3)
Destiled from here:
https://gerrit.omnirom.org/#/c/4249/32

PS2: something

Change-Id: I7a8135818c57929b612f4fcccdc425a49b72ae59

src/com/android/settings/aicp/KeyboardAnimationInterfaceSettings.java
@@ -1,0 +1,249 @@
+/*
+ *  Copyright (C) 2013 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.settings.aicp;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.util.Log;
+import android.text.TextUtils;
+
+import com.android.settings.widget.SeekBarPreferenceCham;
+import com.android.internal.util.aokp.AwesomeAnimationHelper;
+
+public class KeyboardAnimationInterfaceSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "KeyboardAnimationInterfaceSettings";
+
+
+    private static final String IME_ENTER_ANIMATION = "ime_enter_animation";
+    private static final String IME_EXIT_ANIMATION = "ime_exit_animation";
+    private static final String IME_INTERPOLATOR = "ime_interpolator";
+    private static final String IME_ANIM_DURATION = "ime_anim_duration";
+
+    private static final int MENU_RESET = Menu.FIRST;
+
+    private ListPreference mAnimationImeEnter;
+    private ListPreference mAnimationImeExit;
+    private ListPreference mAnimationImeInterpolator;
+    private SeekBarPreferenceCham mAnimationImeDuration;
+
+    private int[] mAnimations;
+    private String[] mAnimationsStrings;
+    private String[] mAnimationsNum;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.keyboard_animation_interface_settings);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mAnimations = AwesomeAnimationHelper.getAnimationsList();
+        int animqty = mAnimations.length;
+        mAnimationsStrings = new String[animqty];
+        mAnimationsNum = new String[animqty];
+        for (int i = 0; i < animqty; i++) {
+            mAnimationsStrings[i] = AwesomeAnimationHelper.getProperName(getActivity().getResources(), mAnimations[i]);
+            mAnimationsNum[i] = String.valueOf(mAnimations[i]);
+        }
+
+        mAnimationImeEnter = (ListPreference) prefSet.findPreference(IME_ENTER_ANIMATION);
+        mAnimationImeEnter.setOnPreferenceChangeListener(this);
+        if (getProperVal(mAnimationImeEnter) != null) {
+             mAnimationImeEnter.setValue(getProperVal(mAnimationImeEnter));
+             mAnimationImeEnter.setSummary(getProperSummary(mAnimationImeEnter));
+        }
+        mAnimationImeEnter.setEntries(mAnimationsStrings);
+        mAnimationImeEnter.setEntryValues(mAnimationsNum);
+
+        mAnimationImeExit = (ListPreference) prefSet.findPreference(IME_EXIT_ANIMATION);
+        mAnimationImeExit.setOnPreferenceChangeListener(this);
+        if (getProperVal(mAnimationImeExit) != null) {
+             mAnimationImeExit.setValue(getProperVal(mAnimationImeExit));
+             mAnimationImeExit.setSummary(getProperSummary(mAnimationImeExit));
+        }
+        mAnimationImeExit.setEntries(mAnimationsStrings);
+        mAnimationImeExit.setEntryValues(mAnimationsNum);
+
+        mAnimationImeInterpolator = (ListPreference) prefSet.findPreference(IME_INTERPOLATOR);
+        if (getProperVal(mAnimationImeInterpolator) != null) {
+             mAnimationImeInterpolator.setValue(getProperVal(mAnimationImeInterpolator));
+             mAnimationImeInterpolator.setSummary(getListInterpolatorName(Integer.valueOf(getProperVal(mAnimationImeInterpolator))));
+        }
+        mAnimationImeInterpolator.setOnPreferenceChangeListener(this);
+
+        int imeDuration = Settings.System.getInt(resolver,
+                Settings.System.ANIMATION_IME_DURATION, 0);
+        mAnimationImeDuration = (SeekBarPreferenceCham) prefSet.findPreference(IME_ANIM_DURATION);
+        mAnimationImeDuration.setValue(imeDuration);
+        mAnimationImeDuration.setOnPreferenceChangeListener(this);
+
+        setHasOptionsMenu(true);
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(R.drawable.ic_settings_backup) // use the backup icon
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                resetToDefault();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private void resetToDefault() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+        alertDialog.setTitle(R.string.reset);
+        alertDialog.setMessage(R.string.animation_settings_reset_message);
+        alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                resetAllValues();
+                resetAllSettings();
+            }
+        });
+        alertDialog.setNegativeButton(R.string.cancel, null);
+        alertDialog.create().show();
+    }
+
+    private void resetAllValues() {
+        mAnimationImeEnter.setValue("0");
+        mAnimationImeExit.setValue("0");
+        mAnimationImeInterpolator.setValue("0");
+        mAnimationImeDuration.setValue(0);
+    }
+
+    private void resetAllSettings() {
+        setProperVal(mAnimationImeEnter, 0);
+        mAnimationImeEnter.setSummary(getProperSummary(mAnimationImeEnter));
+        setProperVal(mAnimationImeExit, 0);
+        mAnimationImeExit.setSummary(getProperSummary(mAnimationImeExit));
+        setProperVal(mAnimationImeInterpolator, 0);
+        mAnimationImeInterpolator.setSummary(getListInterpolatorName(0));
+        setProperVal(mAnimationImeDuration, 0);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mAnimationImeDuration) {
+            int val = ((Integer)objValue).intValue();
+            Settings.System.putInt(resolver,
+                    Settings.System.ANIMATION_IME_DURATION,
+                    val);
+        } else if (preference == mAnimationImeEnter) {
+            int val = Integer.parseInt((String) objValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.ANIMATION_IME_ENTER,
+                    val);
+            mAnimationImeEnter.setSummary(getProperSummary(mAnimationImeEnter));
+        } else if (preference == mAnimationImeExit) {
+            int val = Integer.parseInt((String) objValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.ANIMATION_IME_EXIT,
+                    val);
+            mAnimationImeExit.setSummary(getProperSummary(mAnimationImeExit));
+        } else if (preference == mAnimationImeInterpolator) {
+            int val = Integer.parseInt((String) objValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.ANIMATION_IME_INTERPOLATOR,
+                    val);
+            mAnimationImeInterpolator.setSummary(getListInterpolatorName(val));
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    private void setProperVal(Preference preference, int val) {
+        String mString = "";
+        if (preference == mAnimationImeEnter) {
+            mString = Settings.System.ANIMATION_IME_ENTER;
+        } else if (preference == mAnimationImeExit) {
+            mString = Settings.System.ANIMATION_IME_EXIT;
+        } else if (preference == mAnimationImeInterpolator) {
+            mString = Settings.System.ANIMATION_IME_INTERPOLATOR;
+        } else if (preference == mAnimationImeDuration) {
+            mString = Settings.System.ANIMATION_IME_DURATION;
+        }
+
+        Settings.System.putInt(getActivity().getContentResolver(), mString, val);
+    }
+
+    private String getProperSummary(Preference preference) {
+        String mString = "";
+        if (preference == mAnimationImeEnter) {
+            mString = Settings.System.ANIMATION_IME_ENTER;
+        } else if (preference == mAnimationImeExit) {
+            mString = Settings.System.ANIMATION_IME_EXIT;
+        }
+
+        String mNum = Settings.System.getString(getActivity().getContentResolver(), mString);
+        return AwesomeAnimationHelper.getProperName(getActivity().getResources(), Integer.valueOf(mNum));
+    }
+
+    private String getProperVal(Preference preference) {
+        String mString = "";
+        if (preference == mAnimationImeEnter) {
+            mString = Settings.System.ANIMATION_IME_ENTER;
+        } else if (preference == mAnimationImeExit) {
+            mString = Settings.System.ANIMATION_IME_EXIT;
+        } else if (preference == mAnimationImeInterpolator) {
+            mString = Settings.System.ANIMATION_IME_INTERPOLATOR;
+        }
+
+        return Settings.System.getString(getActivity().getContentResolver(), mString);
+    }
+
+    private String getListInterpolatorName(int index) {
+    	String[] str = getActivity().getResources().getStringArray(R.array.listview_interpolator_entries);
+    	return str[index];
+    }
+
+}

src/com/android/settings/aicp/TestKeyboard.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2013 XuiMod
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.aicp;
+
+import android.content.Context;
+import android.preference.Preference;
+import android.text.InputType;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class TestKeyboard extends Preference{
+
+       int mHint;
+
+       public TestKeyboard(Context context, AttributeSet attrs) {
+           super(context, attrs);
+           mHint = attrs.getAttributeResourceValue(null, "hint", 0);
+       }
+
+       @Override
+       protected View onCreateView(ViewGroup parent){
+           EditText tv = new EditText(this.getContext());
+           tv.setPadding(0, 15, 0, 15);
+           tv.setHint(mHint);
+           tv.setGravity(Gravity.CENTER);
+           tv.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+           return tv;
+       }
+}

szunyi77/android_packages_apps_Settings-1

Conflicts:
	res/values/arrays_aicp.xml
	res/values/strings_aicp.xml
	res/xml/aicp_display_animations_settings.xml
	res/menu/ram_bar_settings.xml
	res/values/liquid_strings.xml
	src/com/android/settings/liquid/StatusBarColor.java

src/com/android/settings/liquid/KeyboardAnimationInterfaceSettings.java
@@ -1,0 +1,249 @@
+/*
+ *  Copyright (C) 2013 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.settings.liquid;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.util.Log;
+import android.text.TextUtils;
+
+import com.android.settings.widget.SeekBarPreferenceCham;
+import com.android.internal.util.liquid.AnimationHelper;
+
+public class KeyboardAnimationInterfaceSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "KeyboardAnimationInterfaceSettings";
+
+
+    private static final String IME_ENTER_ANIMATION = "ime_enter_animation";
+    private static final String IME_EXIT_ANIMATION = "ime_exit_animation";
+    private static final String IME_INTERPOLATOR = "ime_interpolator";
+    private static final String IME_ANIM_DURATION = "ime_anim_duration";
+
+    private static final int MENU_RESET = Menu.FIRST;
+
+    private ListPreference mAnimationImeEnter;
+    private ListPreference mAnimationImeExit;
+    private ListPreference mAnimationImeInterpolator;
+    private SeekBarPreferenceCham mAnimationImeDuration;
+
+    private int[] mAnimations;
+    private String[] mAnimationsStrings;
+    private String[] mAnimationsNum;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.keyboard_animation_interface_settings);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mAnimations = AnimationHelper.getAnimationsList();
+        int animqty = mAnimations.length;
+        mAnimationsStrings = new String[animqty];
+        mAnimationsNum = new String[animqty];
+        for (int i = 0; i < animqty; i++) {
+            mAnimationsStrings[i] = AnimationHelper.getProperName(getActivity().getResources(), mAnimations[i]);
+            mAnimationsNum[i] = String.valueOf(mAnimations[i]);
+        }
+
+        mAnimationImeEnter = (ListPreference) prefSet.findPreference(IME_ENTER_ANIMATION);
+        mAnimationImeEnter.setOnPreferenceChangeListener(this);
+        if (getProperVal(mAnimationImeEnter) != null) {
+             mAnimationImeEnter.setValue(getProperVal(mAnimationImeEnter));
+             mAnimationImeEnter.setSummary(getProperSummary(mAnimationImeEnter));
+        }
+        mAnimationImeEnter.setEntries(mAnimationsStrings);
+        mAnimationImeEnter.setEntryValues(mAnimationsNum);
+
+        mAnimationImeExit = (ListPreference) prefSet.findPreference(IME_EXIT_ANIMATION);
+        mAnimationImeExit.setOnPreferenceChangeListener(this);
+        if (getProperVal(mAnimationImeExit) != null) {
+             mAnimationImeExit.setValue(getProperVal(mAnimationImeExit));
+             mAnimationImeExit.setSummary(getProperSummary(mAnimationImeExit));
+        }
+        mAnimationImeExit.setEntries(mAnimationsStrings);
+        mAnimationImeExit.setEntryValues(mAnimationsNum);
+
+        mAnimationImeInterpolator = (ListPreference) prefSet.findPreference(IME_INTERPOLATOR);
+        if (getProperVal(mAnimationImeInterpolator) != null) {
+             mAnimationImeInterpolator.setValue(getProperVal(mAnimationImeInterpolator));
+             mAnimationImeInterpolator.setSummary(getListInterpolatorName(Integer.valueOf(getProperVal(mAnimationImeInterpolator))));
+        }
+        mAnimationImeInterpolator.setOnPreferenceChangeListener(this);
+
+        int imeDuration = Settings.System.getInt(resolver,
+                Settings.System.ANIMATION_IME_DURATION, 0);
+        mAnimationImeDuration = (SeekBarPreferenceCham) prefSet.findPreference(IME_ANIM_DURATION);
+        mAnimationImeDuration.setValue(imeDuration);
+        mAnimationImeDuration.setOnPreferenceChangeListener(this);
+
+        setHasOptionsMenu(true);
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(R.drawable.ic_settings_backup) // use the backup icon
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                resetToDefault();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private void resetToDefault() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+        alertDialog.setTitle(R.string.reset);
+        alertDialog.setMessage(R.string.animation_settings_reset_message);
+        alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                resetAllValues();
+                resetAllSettings();
+            }
+        });
+        alertDialog.setNegativeButton(R.string.cancel, null);
+        alertDialog.create().show();
+    }
+
+    private void resetAllValues() {
+        mAnimationImeEnter.setValue("0");
+        mAnimationImeExit.setValue("0");
+        mAnimationImeInterpolator.setValue("0");
+        mAnimationImeDuration.setValue(0);
+    }
+
+    private void resetAllSettings() {
+        setProperVal(mAnimationImeEnter, 0);
+        mAnimationImeEnter.setSummary(getProperSummary(mAnimationImeEnter));
+        setProperVal(mAnimationImeExit, 0);
+        mAnimationImeExit.setSummary(getProperSummary(mAnimationImeExit));
+        setProperVal(mAnimationImeInterpolator, 0);
+        mAnimationImeInterpolator.setSummary(getListInterpolatorName(0));
+        setProperVal(mAnimationImeDuration, 0);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mAnimationImeDuration) {
+            int val = ((Integer)objValue).intValue();
+            Settings.System.putInt(resolver,
+                    Settings.System.ANIMATION_IME_DURATION,
+                    val);
+        } else if (preference == mAnimationImeEnter) {
+            int val = Integer.parseInt((String) objValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.ANIMATION_IME_ENTER,
+                    val);
+            mAnimationImeEnter.setSummary(getProperSummary(mAnimationImeEnter));
+        } else if (preference == mAnimationImeExit) {
+            int val = Integer.parseInt((String) objValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.ANIMATION_IME_EXIT,
+                    val);
+            mAnimationImeExit.setSummary(getProperSummary(mAnimationImeExit));
+        } else if (preference == mAnimationImeInterpolator) {
+            int val = Integer.parseInt((String) objValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.ANIMATION_IME_INTERPOLATOR,
+                    val);
+            mAnimationImeInterpolator.setSummary(getListInterpolatorName(val));
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    private void setProperVal(Preference preference, int val) {
+        String mString = "";
+        if (preference == mAnimationImeEnter) {
+            mString = Settings.System.ANIMATION_IME_ENTER;
+        } else if (preference == mAnimationImeExit) {
+            mString = Settings.System.ANIMATION_IME_EXIT;
+        } else if (preference == mAnimationImeInterpolator) {
+            mString = Settings.System.ANIMATION_IME_INTERPOLATOR;
+        } else if (preference == mAnimationImeDuration) {
+            mString = Settings.System.ANIMATION_IME_DURATION;
+        }
+
+        Settings.System.putInt(getActivity().getContentResolver(), mString, val);
+    }
+
+    private String getProperSummary(Preference preference) {
+        String mString = "";
+        if (preference == mAnimationImeEnter) {
+            mString = Settings.System.ANIMATION_IME_ENTER;
+        } else if (preference == mAnimationImeExit) {
+            mString = Settings.System.ANIMATION_IME_EXIT;
+        }
+
+        String mNum = Settings.System.getString(getActivity().getContentResolver(), mString);
+        return AnimationHelper.getProperName(getActivity().getResources(), Integer.valueOf(mNum));
+    }
+
+    private String getProperVal(Preference preference) {
+        String mString = "";
+        if (preference == mAnimationImeEnter) {
+            mString = Settings.System.ANIMATION_IME_ENTER;
+        } else if (preference == mAnimationImeExit) {
+            mString = Settings.System.ANIMATION_IME_EXIT;
+        } else if (preference == mAnimationImeInterpolator) {
+            mString = Settings.System.ANIMATION_IME_INTERPOLATOR;
+        }
+
+        return Settings.System.getString(getActivity().getContentResolver(), mString);
+    }
+
+    private String getListInterpolatorName(int index) {
+    	String[] str = getActivity().getResources().getStringArray(R.array.listview_interpolator_entries);
+    	return str[index];
+    }
+
+}

src/com/android/settings/liquid/TestKeyboard.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2013 XuiMod
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.liquid;
+
+import android.content.Context;
+import android.preference.Preference;
+import android.text.InputType;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class TestKeyboard extends Preference{
+
+       int mHint;
+
+       public TestKeyboard(Context context, AttributeSet attrs) {
+           super(context, attrs);
+           mHint = attrs.getAttributeResourceValue(null, "hint", 0);
+       }
+
+       @Override
+       protected View onCreateView(ViewGroup parent){
+           EditText tv = new EditText(this.getContext());
+           tv.setPadding(0, 15, 0, 15);
+           tv.setHint(mHint);
+           tv.setGravity(Gravity.CENTER);
+           tv.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+           return tv;
+       }
+}