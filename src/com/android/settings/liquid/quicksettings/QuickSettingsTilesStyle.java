cf8c1cc3033f374f4890433c0be8ecd1dd379684
Meticulus <theonejohnnyd@gmail.com>
2014-03-13 13:51:13 -0500

    fix: enabling/disabling "Flip tiles"
    -Not sure exactly which change fixed this...
    
    -OnPreferenceChanged* wasn't hooked up
    
    -Normalized content resolver usage
    
    -Normalized use of "newvalue" onpreferencechanged*
    
    -Change fliptiles setting init to use default "enabled"
    to match SystemUI
    
    -NOTE: There was no error to go off of but when you changed
    the value, it wasn't making it into the database..
    
    Change-Id: Ida073ce97d11a54a71e9cc13a986ca677329362d
    
    Conflicts:
    	src/com/android/settings/liquid/quicksettings/QuickSettingsTilesStyle.java

--- src/com/android/settings/liquid/quicksettings/QuickSettingsTilesStyle.java
+++ src/com/android/settings/liquid/quicksettings/QuickSettingsTilesStyle.java
@@ -96,7 +96,6 @@ public class QuickSettingsTilesStyle extends SettingsPreferenceFragment implemen
         addPreferencesFromResource(R.xml.quicksettings_tiles_style);
 
         prefs = getPreferenceScreen();
-        ContentResolver resolver = getActivity().getContentResolver();
 
         PackageManager pm = getPackageManager();
         Resources systemUiResources;
@@ -166,6 +165,11 @@ public class QuickSettingsTilesStyle extends SettingsPreferenceFragment implemen
             mQsTileAlpha.setInitValue((int) (transparency * 100));
             mQsTileAlpha.setOnPreferenceChangeListener(this);
         }
+        
+		mFlipQsTiles = (CheckBoxPreference) findPreference(PREF_FLIP_QS_TILES);
+        mFlipQsTiles.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.QUICK_SETTINGS_TILES_FLIP, 1) == 1);
+		mFlipQsTiles.setOnPreferenceChangeListener(this);
 
         mTilesPerRow = (ListPreference) findPreference(PREF_TILES_PER_ROW);
         int tilesPerRow = Settings.System.getInt(getActivity().getContentResolver(),
@@ -225,7 +229,6 @@ public class QuickSettingsTilesStyle extends SettingsPreferenceFragment implemen
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getContentResolver();
         if (!mCheckPreferences) {
             return false;
         }
@@ -242,12 +245,17 @@ public class QuickSettingsTilesStyle extends SettingsPreferenceFragment implemen
                     Settings.System.QUICK_TILES_PER_ROW_DUPLICATE_LANDSCAPE,
                     (Boolean) newValue ? 1 : 0);
             return true;
+        } else if (preference == mFlipQsTiles) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.QUICK_SETTINGS_TILES_FLIP,
+                    (Boolean) newValue ? 1 : 0);
+            return true;
         } else if (preference == mQuickTilesBgColor) {
             String hex = ColorPickerPreference.convertToARGB(
                     Integer.valueOf(String.valueOf(newValue)));
             preference.setSummary(hex);
             int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getContentResolver(),
+            Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.QUICK_TILES_BG_COLOR,
                     intHex);
             return true;
@@ -256,7 +264,7 @@ public class QuickSettingsTilesStyle extends SettingsPreferenceFragment implemen
                     Integer.valueOf(String.valueOf(newValue)));
             preference.setSummary(hex);
             int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getContentResolver(),
+            Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.QUICK_TILES_BG_PRESSED_COLOR,
                     intHex);
             return true;
@@ -265,13 +273,13 @@ public class QuickSettingsTilesStyle extends SettingsPreferenceFragment implemen
                     Integer.valueOf(String.valueOf(newValue)));
             preference.setSummary(hex);
             int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getContentResolver(),
+            Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.QUICK_TILES_TEXT_COLOR,
                     intHex);
             return true;
         } else if (preference == mQsTileAlpha) {
             float valNav = Float.parseFloat((String) newValue);
-            Settings.System.putFloat(getContentResolver(),
+            Settings.System.putFloat(getActivity().getContentResolver(),
                     Settings.System.QUICK_TILES_BG_ALPHA, valNav / 100);
             return true;
         }
