dskim9752/android_packages_apps_Settings

Internal Memory Quick Panel

src/com/android/settings/quicksettings/QuickSettingsUtil.java
@@ -30,6 +30,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_LOCKSCREEN;
 import static com.android.internal.util.cm.QSConstants.TILE_LTE;
 import static com.android.internal.util.cm.QSConstants.TILE_MOBILEDATA;
 import static com.android.internal.util.cm.QSConstants.TILE_MUSIC;
+import static com.android.internal.util.cm.QSConstants.TILE_INTERNALMEMORY;
 import static com.android.internal.util.cm.QSConstants.TILE_NETWORKADB;
 import static com.android.internal.util.cm.QSConstants.TILE_NETWORKMODE;
 import static com.android.internal.util.cm.QSConstants.TILE_NFC;
@@ -157,6 +158,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_MUSIC, R.string.title_tile_music,
                 "com.android.systemui:drawable/ic_qs_media_play"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_INTERNALMEMORY, R.string.internal_memory_tile,
+                "com.android.systemui:drawable/ic_qs_internalmemory"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {

szunyi77/android_packages_apps_Settings-1

Conflicts:
	res/values-ko/dokdo_strings.xml
	res/values/dokdo_strings.xml
	src/com/android/settings/quicksettings/QuickSettingsUtil.java
	res/values/liquid_strings.xml
	src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java

src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
@@ -55,6 +55,7 @@ import static com.android.internal.util.liquid.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.liquid.QSConstants.TILE_WIFI;
 import static com.android.internal.util.liquid.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.liquid.QSConstants.TILE_REBOOT;
+import static com.android.internal.util.liquid.QSConstants.TILE_INTERNALMEMORY;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -169,6 +170,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_CONTACT, R.string.title_tile_contact,
                 "com.android.systemui:drawable/ic_qs_default_user"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_INTERNALMEMORY, R.string.internal_memory_tile,
+                "com.android.systemui:drawable/ic_qs_internalmemory"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {