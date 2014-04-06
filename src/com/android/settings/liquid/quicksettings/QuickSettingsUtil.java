DirtyUnicorns/android_packages_apps_Settings

Add Fast Charge QS Tile [2/2]
Change-Id: Iafd5ee0eefa9f7abd41b03ea7fa4e976c573e61c

src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -52,6 +52,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.slim.QSConstants.TILE_NETWORKADB;
 import static com.android.internal.util.slim.QSConstants.TILE_GPS;
+import static com.android.internal.util.slim.QSConstants.TILE_FCHARGE;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -163,6 +164,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_GPS, R.string.title_tile_gps,
                 "com.android.systemui:drawable/ic_qs_gps_off"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_FCHARGE, R.string.title_tile_fcharge,
+                "com.android.systemui:drawable/ic_qs_fcharge_off"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
@@ -220,6 +224,10 @@ public class QuickSettingsUtil {
             removeTile(TILE_NETWORKADB);
         }
 
+        // Don't show the Fast charge tile if not supported by kernel
+        if (!DeviceUtils.fchargeEnabled(context)) {
+            removeTile(TILE_FCHARGE);
+        }
 
     }
 

szunyi77/android_packages_apps_Settings-1

Conflicts:
	res/values/custom_strings.xml
	src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
	src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java

src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
@@ -56,6 +56,7 @@ import static com.android.internal.util.liquid.QSConstants.TILE_WIFI;
 import static com.android.internal.util.liquid.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.liquid.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.liquid.QSConstants.TILE_INTERNALMEMORY;
+import static com.android.internal.util.liquid.QSConstants.TILE_FCHARGE;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -173,6 +174,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_INTERNALMEMORY, R.string.internal_memory_tile,
                 "com.android.systemui:drawable/ic_qs_internalmemory"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_FCHARGE, R.string.title_tile_fcharge,
+                "com.android.systemui:drawable/ic_qs_fcharge_off"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
@@ -224,6 +228,12 @@ public class QuickSettingsUtil {
         if (!DeviceUtils.deviceSupportsTorch(context)) {
             removeTile(TILE_TORCH);
         }
+
+        // Don't show the Fast charge tile if not supported by kernel
+        if (!DeviceUtils.fchargeEnabled(context)) {
+            removeTile(TILE_FCHARGE);
+        }
+
     }
 
     public static ArrayList<String> getAllDynamicTiles(Context context) {