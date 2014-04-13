5b89ae4ab298b5c7fe0adeed1f7ca62da5adcee0
cristianomatos <cristianobmatos@gmail.com>
2014-04-13 13:37:43 -0300

    RAM bar (2/2)
    Change-Id: I0f47445618a991e310923e2b85e180e921c57742
    
    Conflicts:
    	AndroidManifest.xml
    	res/values/cr_arrays.xml
    	res/values/cr_strings.xml
    	res/xml/recents_panel_settings.xml
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1192,5 +1192,5 @@ public class Settings extends PreferenceActivity
     public static class QuickSettingsTilesSettingsActivity extends Settings { /* empty */ }
     public static class BatteryIconStyleSettingsActivity extends Settings { /* empty */ }
     public static class DisplayRotationSettingsActivity extends Settings { /* empty */ }
-    public static class RamBarSettingsActivity extends Settings { /* empty */ }
+    public static class RamBarActivity extends Settings { /* empty */ }
 }
